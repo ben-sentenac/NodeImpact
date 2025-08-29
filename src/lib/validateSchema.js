b2da8ed45de20a255e02775eea30af1dc3c334a2
ben-sentenac <bsr-web-09@proton.me>
2025-08-29 16:00:45 +0200

    chore(src/lib):add Ajv and validateConfigSchema function

---
+++ src/lib/validateSchema.js
@@ -0,0 +1,17 @@
+import Ajv from "ajv";
+import configSchema from '../config.schema.json' with { type: 'json' };
+
+const ajv = new Ajv();
+
+
+export function validateConfigSchema(_config) {
+    const validate = ajv.compile(configSchema);
+    const valid = validate(_config);
+
+    if(!valid) {
+       const errors = validate.errors.map(err => `${err.instancePath} ${err.message}`).join('\n');
+        throw new Error(`La validation du fichier de configuration a échouée :\n${errors}`)
+    }
+
+    return true;
+}
